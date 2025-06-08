ed87af39b7ff6c5877ec979afcb5864293586370
emmileheloise <160171934+emmileheloise@users.noreply.github.com>
2025-06-08 16:30:34 -0300

    Delete agenda-de-manicure directory

--- agenda-de-manicure/controle/agenda-de-manicure/backend/modulo/Cliente.js
+++
@@ -1,9 +0,0 @@
-const Usuario = require('./Usuario');
-
-class Cliente extends Usuario {
-  constructor(id, nome, email, senha, telefone, dataNasc, cpf) {
-    super(id, nome, email, senha, telefone, dataNasc, cpf);
-  }
-}
-
-module.exports = Cliente;
