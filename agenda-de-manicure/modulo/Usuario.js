cf2afae7df419fe032da945ab2913d932db13cce
indyycx <iesb1@aluno.ifal.edu.br>
2025-06-07 23:23:37 -0300

    Update Usuario.js

--- agenda-de-manicure/modulo/Usuario.js
+++ agenda-de-manicure/modulo/Usuario.js
@@ -8,6 +8,14 @@ class Usuario {
   #cpf;
 
   constructor(id, nome, email, senha, telefone, dataNasc, cpf) {
+    if (typeof id !== "number") throw new Error("O ID deve ser um número.");
+    if (typeof nome !== "string") throw new Error("O nome deve ser uma string.");
+    if (typeof email !== "string") throw new Error("O email deve ser uma string.");
+    if (typeof senha !== "string") throw new Error("A senha deve ser uma string.");
+    if (typeof telefone !== "string") throw new Error("O telefone deve ser uma string.");
+    if (typeof dataNasc !== "string") throw new Error("A data de nascimento deve ser uma string.");
+    if (typeof cpf !== "string") throw new Error("O CPF deve ser uma string.");
+
     this.#id = id;
     this.#nome = nome;
     this.#email = email;
@@ -21,6 +29,7 @@ class Usuario {
     return this.#id;
   }
   set Id(novo) {
+    if (typeof novo !== "number") throw new Error("O ID deve ser um número.");
     this.#id = novo;
   }
 
@@ -28,6 +37,7 @@ class Usuario {
     return this.#nome;
   }
   set Nome(novo) {
+    if (typeof novo !== "string") throw new Error("O nome deve ser uma string.");
     this.#nome = novo;
   }
 
@@ -35,6 +45,7 @@ class Usuario {
     return this.#email;
   }
   set Email(novo) {
+    if (typeof novo !== "string") throw new Error("O email deve ser uma string.");
     this.#email = novo;
   }
 
@@ -42,6 +53,7 @@ class Usuario {
     return this.#senha;
   }
   set Senha(novo) {
+    if (typeof novo !== "string") throw new Error("A senha deve ser uma string.");
     this.#senha = novo;
   }
 
@@ -49,6 +61,7 @@ class Usuario {
     return this.#telefone;
   }
   set Telefone(novo) {
+    if (typeof novo !== "string") throw new Error("O telefone deve ser uma string.");
     this.#telefone = novo;
   }
 
@@ -56,6 +69,7 @@ class Usuario {
     return this.#dataNasc;
   }
   set DataNasc(novo) {
+    if (typeof novo !== "string") throw new Error("A data de nascimento deve ser uma string.");
     this.#dataNasc = novo;
   }
 
@@ -63,6 +77,7 @@ class Usuario {
     return this.#cpf;
   }
   set Cpf(novo) {
+    if (typeof novo !== "string") throw new Error("O CPF deve ser uma string.");
     this.#cpf = novo;
   }
 }
