d6a05edf82d6ddb920ce2940726c1ef10454f696
indyycx <iesb1@aluno.ifal.edu.br>
2025-06-07 22:55:24 -0300

    Update AtendimentoController.js

--- agenda-de-manicure/controle/AtendimentoController.js
+++ agenda-de-manicure/controle/AtendimentoController.js
@@ -1,44 +1,46 @@
+
 const Atendimento = require('../modulo/Atendimento');
 
 class AtendimentoController {
-    constructor() {
-        this.atendimentos = [];
-    }
+  constructor() {
+    this.atendimentos = [];
+  }
 
-    adicionarAtendimento(atendimento) {
-        if (atendimento instanceof Atendimento) {
-            this.atendimentos.push(atendimento);
-            console.log(`Atendimento registrado: ${atendimento.resumir()}`);
-        } else {
-            console.log("Erro: objeto inválido.");
-        }
+  adicionarAtendimento(atendimento) {
+    if (atendimento instanceof Atendimento) {
+      this.atendimentos.push(atendimento);
+      console.log(`Atendimento registrado: ${atendimento.resumir()}`);
+    } else {
+      console.log("Erro: objeto inválido.");
     }
+  }
 
-    editarPagamento(index, pago) {
-        try {
-            const atendimento = this.atendimentos[index];
-            if (!atendimento) throw new Error("Atendimento não encontrado.");
-            atendimento.pago = pago;
-            console.log('Pagamento atualizado.');
-        } catch (erro) {
-            console.log(`Erro: ${erro.message}`);
-        }
+  editarPagamento(index, pago) {
+    try {
+      const atendimento = this.atendimentos[index];
+      if (!atendimento) throw new Error("Atendimento não encontrado.");
+      atendimento.Pago = pago; 
+      console.log('Pagamento atualizado.');
+    } catch (erro) {
+      console.log(`Erro: ${erro.message}`);
     }
+  }
 
-    listarAtendimentos() {
-        if (this.atendimentos.length === 0) {
-            console.log("\nNão há atendimentos realizados.");
-        } else {
-            console.log("\nAtendimentos Realizados:");
-            this.atendimentos.forEach((a, i) => {
-                console.log(`\n${i}. ${a.pago ? 'Pago' : 'Não pago'}: ${a.resumir()}`);
-            });
-        }
+  listarAtendimentos() {
+    if (this.atendimentos.length === 0) {
+      console.log("\nNão há atendimentos realizados.");
+    } else {
+      console.log("\nAtendimentos Realizados:");
+      this.atendimentos.forEach((a, i) => {
+        console.log(`\n${i}. ${a.Pago ? 'Pago' : 'Não pago'}: ${a.resumir()}`);
+      });
     }
+  }
 
-    obterAtendimento(index) {
-        return this.atendimentos[index] || null;
-    }
+  obterAtendimento(index) {
+    return this.atendimentos[index] || null;
+  }
 }
 
 module.exports = AtendimentoController;
+
